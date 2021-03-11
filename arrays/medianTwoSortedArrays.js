c5584e1f4dbfbb068a3c2d479064c542370b0b78
john <jgh2102@gmail.com>
2021-03-12 07:47:19 +0900

    interview q and first hard solution

--- arrays/medianTwoSortedArrays.js
+++ arrays/medianTwoSortedArrays.js
@@ -2,7 +2,9 @@
 // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 // Follow up: The overall run time complexity should be O(log (m+n)).
-
+nums1 = [1,3], nums2 = [2,4]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
 
 
 /**
@@ -10,13 +12,13 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
+
     
-};
 
 
 
-function merge(leftArray, rightArray) {
+
+var findMedianSortedArrays = function(leftArray, rightArray) {
     const sorted = [];
     while (leftArray.length > 0 && rightArray.length > 0) {
       const leftItem = leftArray[0];
@@ -40,27 +42,31 @@ function merge(leftArray, rightArray) {
       const value = rightArray.shift();
       sorted.push(value);
     }
-  
-    return sorted
-  }
-  
-  function mergeSort(array) {
-    const length = array.length;
-    if (length == 1) {
-      return array;
+    if (sorted.length % 2 == 0){
+      return (sorted[Math.floor(sorted.length/2)] + sorted[Math.floor(sorted.length/2 - 1)]) / 2
+    } else {
+      return sorted[Math.floor(sorted.length/2)]
     }
-  
-    const middleIndex = Math.ceil(length / 2);
-    let leftArray = array.slice(0, middleIndex);
-    let rightArray = array.slice(middleIndex, length);
-  
-    leftArray = mergeSort(leftArray);
-    rightArray = mergeSort(rightArray);
-  
-    return merge(leftArray, rightArray);
   }
+  
+  console.log(findMedianSortedArrays(nums1, nums2))
+  // function mergeSort(array) {
+  //   const length = array.length;
+  //   if (length == 1) {
+  //     return array;
+  //   }
+  
+  //   const middleIndex = Math.ceil(length / 2);
+  //   let leftArray = array.slice(0, middleIndex);
+  //   let rightArray = array.slice(middleIndex, length);
+  
+  //   leftArray = mergeSort(leftArray);
+  //   rightArray = mergeSort(rightArray);
+  
+  //   return merge(leftArray, rightArray);
+  // }
 
 
-  console.log(merge(['apple', 'tree', 'octopus'], ['diamond', 'cow', 'baby']));
-  console.log(merge([2, 3, 88], [17, 1, 10]));
-  console.log(merge([2, 3, 88], [17, 1, 10]));
+  // console.log(merge(['apple', 'tree', 'octopus'], ['diamond', 'cow', 'baby']));
+  // console.log(merge([2, 3, 88], [17, 1, 10]));
+  // console.log(merge([2, 3, 88], [17, 1, 10]));
---
+++ interviews/razzCodingInterview.js
@@ -0,0 +1,53 @@
+// Write a function that given a string, returns a boolean if it is the same read from the left or right
+// i.e - racecar
+let checkPalindrome = function(string) {
+    let length = string.length;
+    for (let i = 0; i < length / 2; i++) {
+        if (string.charAt(i) !== string.charAt(length -1 - i)){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkPalindrome('racecar'))
+
+
+// * Write a function that given a list of countries, returns the country(s) that occur the least times
+// i.e - ["Argentina", "Cuba", "Chile", "Argentina"] -> ["Cuba", "Chile"]
+let leastFrequent = function(array) {
+    let newArray = [];
+    let count = 0
+    let least = 99999999999;
+    let minItem = [];
+    for(let i=0; i <array.length; i++) {
+        for(let j=0; j<array.length; j++) {
+            if (array[i] === array[j]) {
+                count++;
+            }
+        }
+            
+            if (least >= count) {
+                least = count;
+                
+                minItem.push(array[i])
+            }
+        count = 0;
+    }
+    minItem.shift()
+    return minItem;
+}
+
+console.log(leastFrequent(["Argentina", "Cuba", "Chile", "Argentina"]))
+
+
+
+// * Write a function that given a list of strings, returns the list sorted based on the length of each string
+// i.e - ["programming", "simply", "rocks"] ->
+//   ["rocks", "simply", "programming"]
+  
+ let sorter = function(array) {
+     return array.sort((x,y) => x.length - y.length)
+ }
+
+ console.log(sorter(["programming", "simply", "rocks"]))
