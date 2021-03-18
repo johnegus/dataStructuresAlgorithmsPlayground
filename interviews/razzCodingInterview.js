b65d9ce6b7ad9d58175a0a74142c4837ec744967
Your Name <jgh2102@gmail.com>
2021-03-18 12:19:09 +0900

    finally finished least frequent array

--- interviews/razzCodingInterview.js
+++ interviews/razzCodingInterview.js
@@ -15,35 +15,84 @@ console.log(checkPalindrome('racecar'))
 
 // * Write a function that given a list of countries, returns the country(s) that occur the least times
 // i.e - ["Argentina", "Cuba", "Chile", "Argentina"] -> ["Cuba", "Chile"]
-let leastFrequent = function(array) {
-    array.sort()
-    console.log(array)
-    let res =[]
-    let least = array.length + 1;
-    let currentCount = 1
-    for (let i = 1; i < array.length; i++) {
-        if(array[i] == array[i - 1]){
-            currentCount = currentCount + 1
-        } else {
-            if (currentCount < least){
-            least = currentCount + 1;
-            res.pop()
-            res.push(array[i - 1])
-            } 
-            currentCount = 1
+// let leastFrequent = function(array) {
+//     array.sort()
+//     let res =[]
+//     let unique = [...new Set(array)];
+//     console.log(unique)
+//     let least = array.length + 1;
+//     let currentCount = 1
+//     for (let i = 1; i < array.length; i++) {
+      
+//         if(array[i] == array[i - 1]){
+//             currentCount = currentCount + 1
+//         } else {
+//             if (currentCount < least){
+            
+//             least = currentCount;
+//             res.push(array[i - 1])
+//             } 
+//             currentCount = 1
+//         }
+//         if (currentCount = least){
+//             least = currentCount
+//             res.push(array[i]);
+//         } 
+//     }
+    
+//     return res
+// }
+
+let leastFrequent = function(array){
+    let newSet = [...new Set(array)];
+    console.log(newSet)
+    let count = 0;
+    let numSet = []
+    let finalArray = []
+
+    for (let index = 0; index < newSet.length; index++) {
+        const element = newSet[index];
+        for (let j = 0; j < array.length; j++) {
+            const origElement = array[j];
+            if (origElement === newSet[index])
+                count++;
+            
         }
-       
+        numSet.push(count)
+        count = 0
     }
-    if (currentCount < least){
-        least = currentCount;
-        res.push(array[array.length -1])
+    console.log(numSet)
+    let mathMin = Math.min(...numSet)
+    console.log(mathMin)
+    
+    for (let k = 0; k < numSet.length; k++) {
+        const numElement = numSet[k];
+        if (numElement == mathMin){
+            min = numElement
+            finalArray.push(newSet[k])
+        } 
+        
+
+        
     } 
-    return res
+
+    return finalArray
 }
 
-console.log(leastFrequent(["Argentina", "Cuba", "Chile", "Argentina", "Cuba", "Chile"]))
+console.log(leastFrequent(["Argentina", "Cuba", "Chile", "Argentina"] ))
+
+// const arrToInstanceCountObj = arr => arr.reduce((obj, e) => {
+//     let newArray = []
+//     obj[e] = (obj[e] || 0) + 1;
+//     for (let [key, value] of Object.entries(obj)){
+//         console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+//     }
+//     return obj;
+//   }, {});
 
 
+
+// console.log(arrToInstanceCountObj(["Argentina", "Cuba", "Chile", "Argentina"]))
 
 // * Write a function that given a list of strings, returns the list sorted based on the length of each string
 // i.e - ["programming", "simply", "rocks"] ->
